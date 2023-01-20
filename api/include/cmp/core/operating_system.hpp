268a71ea59ca1caeb29f3e3f5ca4feb7ebb353b3
Daniel T. McGinnis <danieltm64@gmail.com>
2023-01-20 20:50:49 -0300

    Added support for FreeBSD

--- api/include/cmp/core/operating_system.hpp
+++ api/include/cmp/core/operating_system.hpp
@@ -13,6 +13,12 @@
 #   define CMP_OS_WINDOWS
 #   define CMP_OS_NAME "Windows"
 
+#elif defined(__APPLE__) \
+    || defined(__MACH__)
+
+#   define CMP_OS_MACOS
+#   define CMP_OS_NAME "macOS"
+
 #elif defined(__linux__) \
     || defined(linux) \
     || defined(__linux)
@@ -20,15 +26,16 @@
 #   define CMP_OS_LINUX_BASED
 #   define CMP_OS_NAME "Linux-based"
 
-#elif defined(__APPLE__) \
-    || defined(__MACH__)
+#elif defined(__FreeBSD__)
 
-#   define CMP_OS_MACOS
-#   define CMP_OS_NAME "macOS"
+#   define CMP_OS_FREEBSD
+#   define CMP_OS_NAME "FreeBSD"
 
 #endif
 
-#if defined(CMP_OS_LINUX_BASED) || defined(CMP_OS_MACOS)
+#if defined(CMP_OS_MACOS) \
+    || defined(CMP_OS_LINUX_BASED) \
+    || defined(CMP_OS_FREEBSD)
 
 #   define CMP_OS_UNIX_LIKE
 
